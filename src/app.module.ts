787d7461b34a02389414df6f970688ea0804376f
Renat Shukiurov <shukiurov.renat@gmail.com>
2022-07-21 11:24:57 +0300

    TypeORM configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import dbConfig from "./db/config/db.config";
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [dbConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({...configService.get('database')})
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
